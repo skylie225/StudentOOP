cc46dfebfdfccdcee8577342b95ec7bb2787ace9
Kyle Smith <51128382+skylie225@users.noreply.github.com>
2019-06-04 12:06:21 -0400

    Create pointers.cpp

---
+++ my_code/pointers.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "pointers.h"
+
+using namespace std;
+
+void inc_by_value(int n) {
+	++n;
+}
+
+
+void inc_with_pointer(int* iptr) {
+	++(*iptr);
+}
+
+
+void inc_with_reference(int& n) {
+	++n;
+}
+
+
+Thing** create_array_of_things(int n) {
+	Thing** new_array = new Thing*[n];
+	for (int i = 0; i < n; i++) {
+		new_array[i] = new Thing(i);
+	}
+    return new_array;
+}
+
+void print_all_things(Thing** things, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << things[i]-> val << " ";
+	}
+	cout << endl;
+}
+
+void double_all_things(Thing** things, int n) {
+	for (int i = 0; i < n; i++) {
+		things[i]->val *= 2;
+	}
+}
+
+void delete_all_things(Thing** things, int n) {
+	for (int i = 0; i < n; i++) {
+		delete things[i];
+	}
+	delete[] things;
+}
+
+
+void assignTA(Student& s, Student& ta) {
+}
+
+void printTAs(vector<Student> students) {
+}
+
