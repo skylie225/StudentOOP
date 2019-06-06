3d87b368a87e7e34545825948275ee2f090b07cd
Kyle Smith <51128382+skylie225@users.noreply.github.com>
2019-06-06 12:49:58 -0400

    Update weather.cpp

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -5,7 +5,24 @@
 using namespace std;
 
 
+//Date class
 
+Date::Date(int d, int m, int y) {
+	if ((d < 1) || (d > 31)) throw d;
+	if ((m < 1) || (m > 12)) throw m;
+	if ((y < 1800) || (y > 2200)) throw y;
+
+	day = d;
+	month = m;
+	year = y;
+}
+
+ostream& operator<<(std::ostream& os, const Date& date) {
+	os << date.month << "/" << date.day << "/" << date.year;
+	return os;
+}
+
+// GPS coordinates
 std::ostream& operator<< (std::ostream& os, const GPS& gps) {
 	os << " latitude: " << gps.latitude << " longitude: " << gps.longitude << endl;
 	return os;
