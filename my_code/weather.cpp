7ed0e6a67aa56e21ee0cb4e4a818b8b52f934be2
Kyle Smith <51128382+skylie225@users.noreply.github.com>
2019-06-13 13:11:30 -0400

    Update weather.cpp

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -4,54 +4,86 @@
 
 using namespace std;
 
+const double F_TO_C = 5 / 9;
+const double C_TO_F = 9 / 5;
 
-//Date class
+Image::Image(int w, int h, std::string flnm)
+    : width(w), height(h)
+{
+    filename = flnm;
+    image_buf = new char[image_sz()];
+}
+
+// copy constructor:
+Image::Image(const Image& img2) {
+	copy_fields(img2);	
+}
+
+// destructor
+Image::~Image() {
+	if (image_buf != nullptr) delete[] image_buf;
+
+}
+
+// assignment operator:
+Image& Image::operator=(const Image& img2) {
+	if (&img2 != this) {
+		if (image_buf != nullptr) delete[] image_buf;
+		copy_fields(img2);
+	}
+	return *this;
+}
+
+int Image::image_sz() {
+    return width * height;
+}
+
+
+void Image::copy_fields(const Image& img2) {
+	height = img2.height;
+	width = img2.width;
+	filename = img2.filename;
+	image_buf = new char[image_sz()];
+	for (int i = 0; i < image_sz(); i++) {
+		image_buf[i] = img2.image_buf[i];
+	}
+}
+
+
+    /*
+     * Setting `display() = 0` here makes this an abstract
+     * class that can't be implemented.
+     * */
+string Image::display(std::string s) {
+    return "Displaying image " + s;
+}
+
+
 
 Date::Date(int d, int m, int y) {
-	if ((d < 1) || (d > 31)) throw d;
-	if ((m < 1) || (m > 12)) throw m;
-	if ((y < 1800) || (y > 2200)) throw y;
-
-	day = d;
-	month = m;
-	year = y;
+    day = d;
+    month = m;
+    year = y;
 }
 
-ostream& operator<<(std::ostream& os, const Date& date) {
-	os << date.month << "/" << date.day << "/" << date.year;
-	return os;
-}
 
-// GPS coordinates
-std::ostream& operator<< (std::ostream& os, const GPS& gps) {
-	os << " latitude: " << gps.latitude << " longitude: " << gps.longitude << endl;
-	return os;
+double WReading::get_tempF() {
+    return (temperature * C_TO_F) + 32;
 }
 
 
 /*
  * A constructor for weather class.
  * */
-Weather::Weather(std::string nm, GPS loc) : 
-	station_nm(nm), my_loc(loc) {
-
+Weather::Weather(std::string nm, GPS loc) :
+    station_nm(nm), my_loc(loc) {
 }
+
 
 string Weather::get_name() const {
-	return station_nm;
+    return station_nm;
 }
 
-int Weather::get_rating() const {
-	return rating;
-}
-
-
-void Weather::set_rating(int new_rating) {
-	rating = new_rating;
-}
-
-ostream& operator<< (ostream& os, const Weather& w) {
-	//int r = w.get_rating();
-	os << w.get_name() << " rating: " << w.get_rating() << w.my_loc;
-	return os;
+void Weather::add_reading(WReading wr) { 
+	wreadings.push_back(wr);
 }
