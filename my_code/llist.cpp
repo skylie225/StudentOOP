255c669e2db749746a4b62d8412aefe89db698f3
Kyle Smith <51128382+skylie225@users.noreply.github.com>
2019-06-21 12:29:29 -0400

    Update llist.cpp

--- my_code/llist.cpp
+++ my_code/llist.cpp
@@ -9,10 +9,7 @@ using namespace std;
  * Output a Node pointer.
  * */
 ostream& operator<<(ostream& os, const Node* nd) {
-	if (!nd) os << "nullptr" << endl;
-	else {
-		os << "data: " << nd->data << "  " << "next: " << nd->next;
-	}
+	os << nd->data;
     return os;
 }
 
@@ -35,9 +32,13 @@ void add_at_end(Node*& head, int d) {
  * Let's do this recursively!
  * */
 void print_list(ostream& os, const Node* head) {
-	os << head;
-	if (head) print_list(os, head->next);
-	else os << endl;
+	if (!head) {
+		os << "nullptr" << endl;
+	}
+	else {
+		os << head << " --> ";
+		print_list(os, head->next);
+	}
 }
 
 /*
@@ -55,4 +56,62 @@ Node* last(Node* head) {
 	if (!head) return nullptr;
 	else if (!head->next) return head;
 	else return last(head->next);
+}
+
+bool del_head(Node*& head) {
+	if (!head) return false;
+	else {
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+		return true;
+
+	}
+}
+
+
+bool del_tail(Node*& curr) {
+	if (!curr) return false;
+	else {
+		if (curr->next == nullptr) {
+			curr = nullptr;
+			return true;
+		}
+		else {
+			return del_tail(curr->next);
+		}
+	}
+}
+
+
+Node* duplicate(Node* head) {
+	if (!head) {
+		return nullptr;
+	}
+	else {
+		return new Node(head->data, duplicate(head->next));
+	
+	}
+}
+
+
+Node* reverse(Node* curr, Node* new_next) {
+	if (!curr) return new_next;
+	else {
+		return reverse(curr->next, new Node(curr->data, new_next));
+	}
+}
+
+Node* join(Node*& list1, Node* list2) {
+	if (!list1) {
+		list1 = list2;
+		return list1;
+	}
+	else if (!list2) {
+		return list1;
+	}
+	else {
+		last(list1)->next = list2;
+		return list1;
+	}
 }
