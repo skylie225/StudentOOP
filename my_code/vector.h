b7731c85665d21e45bfaf685f026fa669f243966
Kyle Smith <51128382+skylie225@users.noreply.github.com>
2019-06-26 13:01:03 -0400

    Update vector.h

--- my_code/vector.h
+++ my_code/vector.h
@@ -24,3 +24,71 @@ class MyVec {
 void print_vector(const MyVec& v);
 
 bool operator==(MyVec& v1, MyVec& v2);
+
+
+// WITH ITERATOR
+
+//
+//const int DEF_CAPACITY = 10;
+//const int CAPACITY_MULT = 2;
+//
+//class MyVec {
+//	class Iterator {
+//		/*
+//		 * This class supports ranged for loops.
+//		 * It includes:
+//		 * Iterator(int*)
+//		 * operator++
+//		 * operator*
+//		 * operator!=
+//		 * */
+//		friend bool operator!=(Iterator& rhs, Iterator& lhs) {
+//			return rhs.iptr != lhs.iptr;
+//		}
+//
+//
+//	public:
+//		Iterator(int* ip) : iptr(ip) {}
+//
+//		Iterator& operator++() {
+//			iptr++;
+//			return *this;
+//		}
+//
+//		int operator*() { 
+//			return *iptr; 
+//		}
+//
+//	private:
+//		int* iptr;
+//	};
+//
+//public:
+//	MyVec();
+//	MyVec(int sz, int val = 0);
+//
+//	// copy control:
+//	MyVec(const MyVec& v2);
+//	~MyVec();
+//	MyVec& operator=(const MyVec& v2);
+//
+//	void push_back(int val);
+//	int size() const { return sz; }
+//	int operator[](int i) const;
+//	int& operator[](int i);
+//	Iterator begin() const;
+//	Iterator end() const;
+//
+//private:
+//	void copy(const MyVec& v2);
+//	int* data;
+//	int sz;
+//	int capacity;
+//};
+//
+//
+//void print_vector(const MyVec& v);
+//
+//bool operator==(MyVec& v1, MyVec& v2);
+
+
