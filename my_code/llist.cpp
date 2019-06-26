201757772bb4a4c99960b32a75080f4980c1f3c5
Kyle Smith <51128382+skylie225@users.noreply.github.com>
2019-06-26 11:19:48 -0400

    Update llist.cpp

--- my_code/llist.cpp
+++ my_code/llist.cpp
@@ -115,3 +115,133 @@ Node* join(Node*& list1, Node* list2) {
 		return list1;
 	}
 }
+
+
+// TEMPLATE
+//
+//#include <iostream>
+//#include "llist.h"
+//
+//using namespace std;
+//
+//
+//
+///*
+// * Output a Node pointer.
+// * */
+//template <typename DATA>
+//ostream& operator<<(ostream& os, const Node<DATA>* nd) {
+//	os << nd->data;
+//	return os;
+//}
+//
+//
+///*
+// * Add a node to the end of a list.
+// * */
+//template <typename DATA>
+//void add_at_end(Node<DATA>*& head, int d) {
+//	if (!head) {
+//		head = new Node<DATA>(d, nullptr);
+//	}
+//	else {
+//		add_at_end(head->next, d);
+//	}
+//}
+//
+//
+///*
+// * Given the head of a list, print the whole thing.
+// * Let's do this recursively!
+// * */
+//
+//template <typename DATA>
+//void print_list(ostream& os, const Node<DATA>* head) {
+//	if (!head) {
+//		os << "nullptr" << endl;
+//	}
+//	else {
+//		os << head << " --> ";
+//		print_list(os, head->next);
+//	}
+//}
+//
+///*
+// * Add a node at the front of a linked list.
+// * */
+//template <typename DATA>
+//void add_at_front(Node<DATA>*& head, int d) {
+//	head = new Node<DATA>(d, head);
+//}
+//
+//
+///*
+// * Get the last node of a list.
+// * */
+//template <typename DATA>
+//Node<DATA>* last(Node<DATA>* head) {
+//	if (!head) return nullptr;
+//	else if (!head->next) return head;
+//	else return last(head->next);
+//}
+//
+//
+//template <typename DATA>
+//bool del_head(Node<DATA>*& head) {
+//	if (!head) return false;
+//	else {
+//		Node<DATA>* temp = head;
+//		head = head->next;
+//		delete temp;
+//		return true;
+//
+//	}
+//}
+//
+//template <typename DATA>
+//bool del_tail(Node<DATA>*& curr) {
+//	if (!curr) return false;
+//	else {
+//		if (curr->next == nullptr) {
+//			curr = nullptr;
+//			return true;
+//		}
+//		else {
+//			return del_tail(curr->next);
+//		}
+//	}
+//}
+//
+//template <typename DATA>
+//Node<DATA>* duplicate(Node<DATA>* head) {
+//	if (!head) {
+//		return nullptr;
+//	}
+//	else {
+//		return new Node<DATA>(head->data, duplicate(head->next));
+//
+//	}
+//}
+//
+//template <typename DATA>
+//Node<DATA>* reverse(Node<DATA>* curr, Node<DATA>* new_next) {
+//	if (!curr) return new_next;
+//	else {
+//		return reverse(curr->next, new Node<DATA>(curr->data, new_next));
+//	}
+//}
+//
+//template <typename DATA>
+//Node<DATA>* join(Node<DATA>*& list1, Node<DATA>* list2) {
+//	if (!list1) {
+//		list1 = list2;
+//		return list1;
+//	}
+//	else if (!list2) {
+//		return list1;
+//	}
+//	else {
+//		last(list1)->next = list2;
+//		return list1;
+//	}
+//}
